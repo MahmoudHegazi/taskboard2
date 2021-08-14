d32e9a4a5cd2129bf516d7c90833d0a84bfe074d
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-08-14 22:51:39 +0200

    Update render.php

--- render.php
+++ render.php
@@ -1,7 +1,5 @@
 <?php
 require('config.php');
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,10 +11,20 @@ require('config.php');
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
         <style>
+        @font-face {
+          font-family: 'Open Sans';
+          font-style: italic;
+          font-weight: 800;
+          src: local('Open Sans ExtraBold'), local('OpenSans-ExtraBold'), url('assets/fonts/OpenSans-ExtraBold.ttf');
+        }
          html, body {
          overflow: auto;
          width: auto;
+         font-size: 1rem;
+         font-family: Verdana, Tahoma, "Trebuchet MS", "DejuVu Sans", "Bitstream Vera Sans", sans-serif;
 
          }
          div.main-container {
@@ -47,68 +55,121 @@ require('config.php');
            border:none;
            color: white;
          }
+         .teamtitle {
+           font-weight: 600;
+           font-size: .825em;
+           background: rgb(12, 200, 50);
+           border: 1px solid white;
+           border-radius: 8px;
+
+           padding: 5px;
+           color: white;
+         }
          tr.week-head{
            width: 100%;
          }
+         .week-real-container {
+            height: 600px;
+            overflow: auto;
+         }
+         .custom_font_opensans {
+           font-family: 'Open Sans';
+           letter-spacing: 1px;
 
-
+         }
          .lightgreen {
            background: #64d891;
          }
          .purple {
            background: #ff0066;
          }
+
+
+
       </style>
 </head>
 <body>
 
   <?php
-  $sql = "SELECT  user.id, user.name, day.id AS 'day_id', day.title AS 'day_title', (SELECT team.title FROM team WHERE team.id = 2) AS 'groupname' FROM user INNER JOIN day WHERE user.team_id = 2 AND day.week_id = 1";
+    echo "<style>";
+    $cssClass = "SELECT classname, color  FROM label_class;";
+    $cssResult = $conn->query($cssClass);
+    if ($cssResult->num_rows > 0) {
+      while( $rowCss = $cssResult->fetch_assoc() ){
+        echo "." . $rowCss['classname'] . "{ " . $rowCss['color'] . "}";
+      }
+
+    }
+
+    echo "</style>";
+  ?>
+
+  <?php
+  // display table
+  $mytable = '
+  <div class="container-fluid mt-4  main-container">
+    <div class="d-flex p-3 text-white month-container week-real-container">';
+
+  // show weeks tables
+  $weeksQuery = "SELECT id, week_name FROM week";
+  $weeksResult = $conn->query($weeksQuery);
+  if ($weeksResult->num_rows > 0) {
+
+    while( $weekrow = $weeksResult->fetch_assoc() ) {
+      $selectedweekid = $weekrow['id'];
+      echo "<!-- Week-" . $weekrow['week_name'] .  "-->";
+
+  $mytable .= '
+    <table class="table table-bordered week-container mr-3">
+      <caption class="bg-info text-center text-white text-justify text-uppercase font-weight-bold custom_font_opensans">' . $weekrow['week_name'] .  '</caption>
+      <thead>
+        <tr class="week-head">
+          <th class="text-center">Teams</th>
+          <th class="text-center">Sunday</th>
+          <th class="text-center">Monday</th>
+          <th class="text-center">Tuesday</th>
+          <th class="text-center">Wednesday</th>
+          <th class="text-center">Thursday</th>
+          <th class="text-center">Friday</th>
+          <th class="text-center">Saturday</th>
+        </tr>
+     </thead>
+    <tbody>';
+
+  $sqlnoproblem = "SELECT id FROM team";
+  $teamidResult = $conn->query($sqlnoproblem);
+if ($teamidResult->num_rows > 0) {
+
+  while($temarow = $teamidResult->fetch_assoc()) {
+    $selectedteamid = $temarow['id'];
+  $sql = "SELECT  user.id, user.name, day.id AS 'day_id', day.title AS 'day_title', (SELECT team.title FROM team WHERE team.id = $selectedteamid) AS 'groupname', (SELECT team.classname FROM team WHERE team.id = $selectedteamid) AS 'teamclass' FROM user INNER JOIN day WHERE day.week_id = 1 AND user.team_id = $selectedteamid";
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
     // output data of each row
-
-    # display table
-    $mytable = '
-    <div class="container-fluid mt-4  main-container">
-      <table class="table table-bordered week-container">
-        <thead>
-          <tr class="week-head">
-            <th class="text-center">Teams</th>
-            <th class="text-center">Sunday</th>
-            <th class="text-center">Monday</th>
-            <th class="text-center">Tuesday</th>
-            <th class="text-center">Wednesday</th>
-            <th class="text-center">Thursday</th>
-            <th class="text-center">Friday</th>
-            <th class="text-center">Saturday</th>
-          </tr>
-       </thead>
-      <tbody>';
     $dayindex = 0;
     $lastGroup = '';
     $kmlhab2a = 0;
     while($row = $result->fetch_assoc()) {
-      if ($dayindex % 7 == 0 && $lastGroup == '' || $lastGroup != $row['groupname']) {
+      if ($dayindex % 6 == 0 && $lastGroup == '' || $lastGroup != $row['groupname']) {
         $lastGroup = $row['groupname'];
         $mytable .= '
         <!-- team start -->
         <!-- teamHeader Start -->
-        <tr class="team-row">
-          <td class="text-center team-header lightgreen"><h5>' . $row["groupname"] . '</h5></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
-          <td class="team-header lightgreen"></td>
+        <tr class="team-row ' . $row['teamclass'] .'">
+          <td class="text-center team-header"><h6 class="teamtitle">' . $row["groupname"] . '</h6></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
+          <td class="team-header"></td>
         </tr>
         <!-- teamhederend -->';
 
       }
       if ($kmlhab2a == 0 || $kmlhab2a != $row['id']) {
-        $mytable .= '<tr class="member_week_row"><td class="member-container lightgreen">' . $row['name'] . '</td>';
+        $mytable .= '<tr class="member_week_row"><td class="member-container ' . $row['teamclass'] . '">' . $row['name'] . '</td>';
         $kmlhab2a = $row['id'];
       }
       $mytable .= '<td class="day-container bg-info "><div class="task-card"></div></td>';
@@ -118,396 +179,25 @@ require('config.php');
 
       $dayindex += 1;
      }
-    } else {
-      echo "0 results";
     }
+
+    /* END of Memeber query while */
+}
+  // team query while end
+}
+  // end of team query while
+
+    $mytable .= '</table>';
+
+  }
+}
+/* End of week query and while */
+
+    $mytable .= '</div></div>';
     echo $mytable;
     $conn->close();
     die();
   ?>
-
-<div class="container-fluid mt-4  main-container">
-  <table class="table table-bordered week-container">
-    <thead>
-      <tr class="week-head">
-        <th class="text-center">Teams</th>
-        <th class="text-center">Sunday</th>
-        <th class="text-center">Monday</th>
-        <th class="text-center">Tuesday</th>
-        <th class="text-center">Wednesday</th>
-        <th class="text-center">Thursday</th>
-        <th class="text-center">Friday</th>
-        <th class="text-center">Saturday</th>
-      </tr>
-    </thead>
-    <tbody>
-
-      <!-- team start -->
-      <tr class="team-row">
-        <td class="text-center team-header lightgreen"><h5>Team1</h5></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-        <td class="team-header lightgreen"></td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container lightgreen">
-          Member 1
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container lightgreen">
-          Member 2
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container lightgreen">
-          Member 3
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container lightgreen">
-          Member 4
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-      <!-- Team End -->
-
-     <!-- team start -->
-      <tr class="team-row">
-        <td class="text-center team-header purple"><h5>Teem2</h5></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-        <td class="team-header purple"></td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container purple">
-          Member 1
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container purple">
-          Member 2
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container purple">
-          Member 3
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-
-      <tr>
-        <td class="member-container purple">
-          Member 4
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-warning ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-primary ">
-          <div class="task-card">
-
-          </div>
-        </td>
-        <td class="day-container bg-info ">
-          <div class="task-card">
-
-          </div>
-        </td>
-      </tr>
-
-      <!-- Team End -->
-    </tbody>
-  </table>
-</div>
 
 </body>
 </html>
